53dae35f26d279bb8a09b8d7ed89057a2903e5cd
Carmel Fred PATCHELLI <40575184+fredpatch@users.noreply.github.com>
2026-08-25 10:54:04 +0200

    feat: bind auth foundation to express app

---
+++ packages/server/src/index.ts
@@ -0,0 +1,2 @@
+export { createServerApp, type ServerAppDeps } from "./app.js";
+export { createExpressAuthRouter, type ExpressAuthRouterDeps, type ExpressAuthSessionResolver } from "./modules/auth/routes/express-auth.router.js";
